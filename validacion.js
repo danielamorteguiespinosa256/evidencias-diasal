6551bea74e06fbfa261ccf4885802d98518c93b9
ALV_KIMURA <danielamorteguiespinosa256@gmail.com>
2026-02-17 10:58:29 -0500

    primer commit

--- validacion.js
+++ validacion.js
@@ -1,24 +1,25 @@
-// let usuario = document.getElementById("usuario")
-//  usuario.addEventListener("keydown",function(evento){
-//    if(evento.keyCode === 8){ 
-//     evento.preventDefault()
-//     this.value += "borrando"
-//    }
-//  })
-let usuario = document.getElementById("usuario")
-let mensaje = document.getElementById("mensaje")
- usuario.addEventListener("keydown",function(evento){
-   this.value = this.value.toLowerCase()
-   this.value =this.value.replace(/[^a-z]/g,"")
-   this.borderColor = "2px solid"
-   if(this.value == ""){
+let usuario = document.getElementById("usuario");
+let mensaje = document.getElementById("mensaje");
 
-    
-      mensaje.textContent = "usuario invalido"
-      this.style.borderColor = "red"
-   }
-   else {
-      mensaje.textContent = "usuario valido"
-       this.style.borderColor = "green"
-   }
- })
+usuario.addEventListener("input", function () {
+
+    let valor = this.value;
+
+    if (/[^a-zA-Z]/.test(valor)) {
+        this.style.border = "2px solid red";
+        mensaje.textContent = "Usuario incorrecto";
+        mensaje.style.color = "red";
+    } 
+    else if (valor === "") {
+        this.style.border = "2px solid red";
+        mensaje.textContent = "Campo obligatorio";
+        mensaje.style.color = "red";
+    } 
+    else {
+        this.style.border = "2px solid green";
+        mensaje.textContent = "Usuario válido";
+        mensaje.style.color = "green";
+    }
+
+    this.value = valor.replace(/[^a-zA-Z]/g, '');
+});
